bf8534dd867ae55f4478e3a43498b3a3b361dba6
VanBinh16 <www.mailacuanhau@gmail.com>
2022-11-18 14:05:34 +0700

    đặt lại tên FOOTER Văn Bình 18/11/2022

--- Frontend/src/views/Index.vue
+++ Frontend/src/views/Index.vue
@@ -6,21 +6,21 @@
       <router-view> </router-view>
       <v-snackbar-ex />
     </v-content>
-    <Footer />
+    <dashboard-core-footer />
   </v-app>
 </template>
 
 <script>
 import DashboardCoreDrawer from "@/components/core/Drawer";
 import DashboardCoreSettings from "@/components/core/Settings";
-import Footer from "@/components/core/Footer";
+import DashboardCoreFooter from "@/components/core/Footer";
 export default {
   name: "DashboardIndex",
 
   components: {
     DashboardCoreDrawer,
     //DashboardCoreSettings,
-    Footer,
+    DashboardCoreFooter,
   },
 
   data: () => ({
